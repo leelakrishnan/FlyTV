b8ad13406b98bb86a5e1b49218df50b8c30b788e
lsubramaniam <leelakrishnan.subramaniam@gmail.com>
2022-03-16 01:09:44 -0400

    Added and removed team member

--- my-app/components/TeamMembers.tsx
+++ my-app/components/TeamMembers.tsx
@@ -2,51 +2,82 @@ import { useState, useEffect } from "react";
 import {useMoralis} from "react-moralis";
 import { toast } from 'react-toastify';
 import styles from "../styles/Form.module.css";
-import { useRouter } from 'next/router'
 import Moralis from "moralis";
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-
+import MaterialTable from "material-table";
 type Props = {
     teamData: any,
 }
-
 const TeamMembers = ({ teamData }: Props)  => {
-    const router = useRouter();
+    const options = {
+        filterType: 'checkbox',
+    };
 
-    // @ts-ignore
-    // @ts-ignore
-    // @ts-ignore
     const [formValues, setFormValues] = useState({
         membersIds: [],
-        members:  []
+        members: [],
+        columns: [
+        ],
+        data: [],
+        emailAddress: ""
     });
+    const [columns, setColumns] = useState([]);
+    const [data, setData] = useState([]);
+
+
     const [formErrors, setFormErrors] = useState({});
     const [loading, setLoading] = useState(false);
     const {user, setUserData, userError, isUserUpdating, refetchUserData} = useMoralis();
     const [loader, setLoader] = useState("not-loaded");
 
-    useEffect(() => {
+    function loadTeamInfo() {
         async function getMemberInfo(membersIds: string[]) {
             const userQuery = new Moralis.Query(Moralis.User);
             userQuery.containedIn("objectId",
                 membersIds);
-            const data = await userQuery.find({ useMasterKey: true });
-            debugger;
+            const data = await userQuery.find({useMasterKey: true});
+            formValues.columns = [
+                {title: 'FirstName', field: 'firstName'},
+                {title: 'LastName', field: 'lastName'},
+                {title: 'Email', field: 'emailAddress'},
+                {title: 'GitHubUsername', field: 'githubUserName'},
+                {title: 'UserId', field: 'userId'}
+
+            ];
+            formValues.data = [];
             if (data) {
                 // @ts-ignore
                 formValues.members = data;
+                // @ts-ignore
+                for (let i = 0; i < data.length; i++) {
+                    let firstName = data[i]?.get("firstName") ? data[i]?.get("firstName") : "";
+                    let lastName = data[i]?.get("lastName") ? data[i]?.get("lastName") : "";
+                    let emailAddress = data[i]?.get("emailAddress") ? data[i]?.get("emailAddress") : "";
+                    let githubUserName = data[i]?.get("githubUserName") ? data[i]?.get("githubUserName") : "";
+                    let userId = data[i]?.id ? data[i]?.id : "";
+
+                    formValues.data.push({
+                        firstName: firstName,
+                        lastName: lastName,
+                        emailAddress: emailAddress,
+                        githubUserName: githubUserName,
+                        userId: userId
+                    });
+                    // @ts-ignore
+                    setData(formValues.data);
+                }
             }
         }
         const memberIds = teamData?.get("users");
         if (memberIds) {
             formValues.membersIds = memberIds;
-            getMemberInfo(memberIds);
+            getMemberInfo(memberIds).then((value) => {
+                setLoader("loaded");
+            });
         }
-        setLoader("loaded");
+    }
+
+    useEffect(() => {
+        loadTeamInfo();
     }, [user]);
 
     const formErrorStyle = {
@@ -62,11 +93,80 @@ const TeamMembers = ({ teamData }: Props)  => {
 
     const validateError = () => {
         const errors = {};
-        if (formValues.repoName === "") {
-            errors.repoName = "RepoName is required";
+        if (formValues.emailAddress === "") {
+            errors.emailAddress = "EmailAddress is required";
         }
         return errors;
     };
+
+    const getUserNotAvailableError = () => {
+        const errors = {};
+        errors.emailAddress = "User is not available in our system";
+        return errors;
+    };
+
+    const getUserAlreadyInTeamError = () => {
+        const errors = {};
+        errors.emailAddress = "User is already in the team";
+        return errors;
+    };
+
+    // @ts-ignore
+    async function getTeamForUser(userId: string) {
+        const Team = Moralis.Object.extend("Team");
+        const query = new Moralis.Query(Team);
+        query.equalTo("users", userId);
+        const data = await query.find({useMasterKey: true});
+        return data;
+    }
+
+    async function getUserInfoByEmail(emailAddress: string) {
+        const userQuery = new Moralis.Query(Moralis.User);
+        userQuery.equalTo("emailAddress",emailAddress);
+        const data = await userQuery.find({useMasterKey: true});
+        return data;
+    }
+
+    async function addUserToTeam(userId: any) {
+        const myTeam = Moralis.Object.extend("Team");
+        const myTeamObj = new myTeam();
+        myTeamObj.set("id", teamData.id);
+        let memberIds = teamData?.get("users");
+        if (memberIds) {
+        } else {
+            memberIds = [];
+        }
+        memberIds.push(userId);
+        myTeamObj.save();
+        toast.success("User Added to Team!", {
+            position: toast.POSITION.BOTTOM_CENTER,
+        });
+    }
+
+    async function deleteUser(event: any, rowData: any) {
+        console.log("deleteUser");
+        console.dir(rowData);
+        event.preventDefault();
+        let userId = rowData?.userId;
+        if(userId) {
+            const memberIds = teamData?.get("users");
+            if (memberIds) {
+                for( let i = 0; i < memberIds.length; i++){
+                    if ( memberIds[i] === userId) {
+                        memberIds.splice(i, 1);
+                    }
+                }
+                const myTeam = Moralis.Object.extend("Team");
+                const myTeamObj = new myTeam();
+                myTeamObj.set("id", teamData.id);
+                myTeamObj.set("users", memberIds);
+                myTeamObj.save();
+                toast.success("User Removed to Team!", {
+                    position: toast.POSITION.BOTTOM_CENTER,
+                });
+            }
+        }
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -81,22 +181,44 @@ const TeamMembers = ({ teamData }: Props)  => {
         validateError();
         console.log("formValues", formValues);
         setLoading(true);
+        // check if email entered is in User
+        const userInfo = await getUserInfoByEmail(formValues.emailAddress);
+        if (userInfo && userInfo.length > 0) {
+            if (userInfo && userInfo[0] && userInfo[0].id) {
+                const teamData = await getTeamForUser(userInfo[0].id);
+                if (teamData && teamData.length > 0) {
+                    const errors = getUserAlreadyInTeamError();
+                    if (Object.keys(errors).length > 0) {
+                        setFormErrors(errors);
+                        setLoading(false);
+                    }
+                } else {
+                    await addUserToTeam(userInfo[0].id);
+                    let firstName = teamData[0]?.get("firstName") ? teamData[0]?.get("firstName") : "";
+                    let lastName = teamData[0]?.get("lastName") ? teamData[0]?.get("lastName") : "";
+                    let emailAddress = teamData[0]?.get("emailAddress") ? teamData[0]?.get("emailAddress") : "";
+                    let githubUserName = teamData[0]?.get("githubUserName") ? teamData[0]?.get("githubUserName") : "";
+                    let userId = userInfo[0].id ? userInfo[0].id: "";
+                    formValues.data.push({
+                        firstName: firstName,
+                        lastName: lastName,
+                        emailAddress: emailAddress,
+                        githubUserName: githubUserName,
+                        userId: userId
+                    });
+                    setData(formValues.data);
 
-
-
-
-        const myTeam = Moralis.Object.extend("Team");
-        const myTeamObj = new myTeam();
-        myTeamObj.set("id", teamData.id);
-        if (formValues.repoName) {
-            myTeamObj.set("repoName", formValues.repoName);
+                }
+            }
+        } else {
+            const errors = getUserNotAvailableError();
+            if (Object.keys(errors).length > 0) {
+                setFormErrors(errors);
+            }
         }
-        myTeamObj.save();
-        toast.success(" Github Info Saved!", {
-            position: toast.POSITION.BOTTOM_CENTER,
-        });
         setLoading(false);
     };
+
 
     return (
         <>
@@ -104,24 +226,16 @@ const TeamMembers = ({ teamData }: Props)  => {
                 {loader == "loaded" &&
                     <form className={styles.form}>
                         <div className={styles.formGroups}>
-                            {formErrors.repoName && (
-                                <p style={formErrorStyle}>{formErrors.repoName}</p>
+                            {formErrors.emailAddress && (
+                                <p style={formErrorStyle}>{formErrors.emailAddress}</p>
                             )}
                             <label htmlFor="name">Add Team Member</label>
                             <input
                                 type="text"
-                                value={formValues.repoName}
-                                name={Object.keys(formValues)[0]}
+                                value={formValues.emailAddress}
+                                name={'emailAddress'}
                                 onChange={handleOnChange}
-                                placeholder="Search And Add Team Member"
-                            />
-                        </div>
-                        <div className={styles.formGroups}>
-                            <input
-                                type="text"
-                                value={formValues.repoName}
-                                name={Object.keys(formValues)[0]}
-                                disabled={true}
+                                placeholder="Enter Email of the Team member to add"
                             />
                         </div>
                         {!loading ? (
@@ -135,6 +249,27 @@ const TeamMembers = ({ teamData }: Props)  => {
                                 <div className="loader"></div>
                             </div>
                         )}
+                        {loader == "loaded" && data &&
+                            <div className={styles.formGroups}>
+                                <MaterialTable
+                                    title="Team members"
+                                    columns={formValues.columns}
+                                    data={data}
+                                    // other props
+                                    actions={[
+                                        {
+                                            icon: 'Delete',
+                                            tooltip: 'Delete User',
+                                            onClick: (event, rowData) => {
+                                                console.dir(rowData);
+                                               deleteUser(event, rowData);
+                                            }
+                                        }
+                                    ]}
+                                />
+                            </div>
+                        }
+
                     </form>
                 }
             </div>
