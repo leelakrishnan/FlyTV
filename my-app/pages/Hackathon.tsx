89f0fb736a062b383f9da2a49e6a8e01cc98e438
lsubramaniam <leelakrishnan.subramaniam@gmail.com>
2022-03-17 18:52:24 -0400

    fixed buttons in hackathon

--- my-app/pages/Hackathon.tsx
+++ my-app/pages/Hackathon.tsx
@@ -39,20 +39,11 @@ const Team: NextPage = () => {
       </Head>
       <main>
         <Nav />
-        <div className={styles.pagetooltip}>
-          <a data-tip=' At the entrance to many hackathons, we ask the question:
-              How many hackathons can you be in at once? Each Hackathon
-              is different, so are the Admin who can access to commit their
-              terms and conditions
-              main missions, and adjust the required profile fields and
-              milestones.'>Help</a>
-          <ReactTooltip className='extraClass' delayHide={1000} effect='solid'/>
-        </div>
         <div className={styles.hero}>
           <div className={styles.container2} style={{ height: "500px" }}>
             <div className={styles.container3}>
               <button
-                className={styles.join}
+                className={styles.teamjoin}
                 onClick={(e) => {
                   takeToTeam(e);
                 }}
@@ -60,7 +51,7 @@ const Team: NextPage = () => {
                 Faber Hackathon 🚀 <br /> Stellar Hackathon
               </button>
               <button
-                className={styles.join}
+                className={styles.teamjoin}
                 onClick={(e) => {
                   takeToTeam(e);
                 }}
@@ -68,7 +59,7 @@ const Team: NextPage = () => {
                 wen slackathon nft?
               </button>
               <button
-                className={styles.join}
+                className={styles.teamjoin}
                 onClick={(e) => {
                   takeToTeam(e);
                 }}
